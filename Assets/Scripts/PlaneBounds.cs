d6d5d1f849960dcfc55f343be4483b7bd33c746b
Kratosflare <55363044+Kratosflare@users.noreply.github.com>
2020-10-18 15:01:20 -0500

    added movement script and modified the plane layout
    More of an open space to move

---
+++ Assets/Scripts/PlaneBounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneBounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // detect the player
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(CompareTag("Player"))
+        {
+            Debug.Log("PLayer entered the fray");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   float horizontalInput;
+   float verticalInput;
+   // private Rigidbody rigidbody;
+   float speed = 5.0f;
+    void Start()
+    {
+       // rigidbody = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+
+        // stop movement when at the edge of the plane
+
+    }
+}
